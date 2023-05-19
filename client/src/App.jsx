7225d321504f61e274a1299d246910060dd77425
Siegfried <siegfried.bozza@renuo.ch>
2023-05-19 15:55:06 +0200

    add App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container my-5">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                defaultValue={userInput.name}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                defaultValue={userInput.email}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                defaultValue={userInput.password}
+            />
+          </div>
+        </form>
+      </div>
   );
 }
 
