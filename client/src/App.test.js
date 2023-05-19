e1c0ddd139240e4d700c47075767b65ee781085e
Siegfried <siegfried.bozza@renuo.ch>
2023-05-19 15:54:45 +0200

    add App.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+
+  test('inputs should be empty on initial render', () => {
+    render(<App />);
+    const nameInputElement = screen.getByRole('textbox', { name: /name/i });
+    const emailInputElement = screen.getByLabelText(/email/i);
+    const passwordInputElement = screen.getByLabelText(/password/i);
+    expect(nameInputElement).toBeInTheDocument();
+    expect(nameInputElement.value).toBe('');
+    expect(emailInputElement).toBeInTheDocument();
+    expect(emailInputElement.value).toBe('');
+    expect(passwordInputElement).toBeInTheDocument();
+    expect(passwordInputElement.value).toBe('');
+  });
+
+})
+
+
